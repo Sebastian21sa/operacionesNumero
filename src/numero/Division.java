63dbd965d514ee730524bfa8332d1f8b229e592d
Sebastian Sanchez <Sebastian Sanchez@DESKTOP-GCBTC4I>
2022-09-23 19:57:45 -0500

    1.0

---
+++ src/numero/Division.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package numero;
+
+/**
+ *
+ * @author Sebastian Sanchez
+ */
+public class Division implements IInterfaces{
+
+    @Override
+    public double operaciones(double num) {
+        return(num/num);
+    }
+    
+}
